ac428145b173ebc73aba9b1aa4b5877008d1b0f6
Sahil Grover <gr.sahil20@gmail.com>
2020-12-15 16:48:07 +0100

    find_max_consecutive_ones

---
+++ arrays/introduction/find_max_consecutive_ones.rb
@@ -0,0 +1,15 @@
+def find_max_consecutive_ones(nums)
+  max_count = 0
+  current_count = 0
+  nums.each do |num|
+    if num === 1
+      current_count += 1
+    else
+      max_count = max_count > current_count ? max_count : current_count
+      current_count = 0
+    end
+  end
+  max_count = max_count > current_count ? max_count : current_count
+end
+
+p find_max_consecutive_ones([1,1,0,1,1,1]) === 3
